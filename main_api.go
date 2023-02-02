abe910e5daa57b2ec9e4041165906cc59cd6c682
Grom <grom2025@gmail.com>
2023-02-02 16:10:57 +0300

    Ref

--- main_api.go
+++ main_api.go
@@ -3,34 +3,42 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
+	http "net/http"
 )
+
+func readDataFrom1C(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		// in case of any error
+		log.Fatalln("Parse Error ", r)
+	}
+	isGroup := r.Form.Get("PAR")
+	name := r.Form.Get("Name")
+	code := r.Form.Get("Code")
+	group := r.Form.Get("Group")
+
+	desc := r.Form.Get("Desc")
+	price := r.Form.Get("Price")
+	quantity := r.Form.Get("Quantity")
+
+	// debug code
+	log.Println(isGroup, code, group, name, desc, price, quantity)
+
+	_, err = fmt.Fprintf(w, "OK")
+	if err != nil {
+		return
+	}
+
+}
 
 func main() {
 	// handle route using handler function
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err := r.ParseForm()
-		if err != nil {
-			// in case of any error
-			log.Fatalln("Parse Error ", r)
-		}
-		isGroup := r.Form.Get("PAR")
-		name := r.Form.Get("Name")
-		code := r.Form.Get("Code")
-		group := r.Form.Get("Group")
-		var desc, price, qantity string
-
-		if isGroup == "0" {
-			desc = r.Form.Get("Desc")
-			price = r.Form.Get("Price")
-			qantity = r.Form.Get("Qantity")
-		}
-		// x will be "" if parameter is not set
-		fmt.Println(code, group, name, desc, price, qantity)
-		//fmt.Fprintf(w, "Welcome to new server!")
-	})
+	http.HandleFunc("/", readDataFrom1C)
 
 	// listen to port
-	http.ListenAndServe(":8000", nil)
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Println("Server launch error")
+	}
 
 }
